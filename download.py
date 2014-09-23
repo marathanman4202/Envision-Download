c4d1945db6953f6fe14c1803ec3e87dbefc95fa8
marathanman4202 <roy.haggerty@oregonstate.edu>
2014-09-23 09:09:09 -0700

    Download script and readme
    This script downloads the WW2100 csv files, unzips them, and puts them
    where you want them.

---
+++ download.py
@@ -0,0 +1,46 @@
+# -*- coding: utf-8 -*-
+"""
+Created on Sat Sep 13 21:50:40 2014
+
+@author: haggertr
+"""
+
+import urllib2
+import zipfile
+from StringIO import StringIO
+
+# Download latest csv files for Envision, and save them to a local directory.
+
+#directory_path = "C:\\Users\\haggertr\\Desktop\\Roy\\Research\\WW2100\\Research\\results2\\files\\"
+directory_path = "C:\\Users\\haggertr\\Desktop\\Roy\\Research\\WW2100\\Research\\results2\\files\\"
+
+response = urllib2.urlopen('http://envision.bioe.orst.edu/StudyAreas/WW2100/Outputs/Ref/Model_Outputs_Ref_Run0.zip')
+zipdata = response.read()
+with zipfile.ZipFile(StringIO(zipdata), "r") as z:
+    z.extractall(directory_path)
+    
+response = urllib2.urlopen('http://envision.bioe.orst.edu/StudyAreas/WW2100/Outputs/LowClim/Model_Outputs_LowClim_Run0.zip')
+zipdata = response.read()
+with zipfile.ZipFile(StringIO(zipdata), "r") as z:
+    z.extractall(directory_path)
+    
+response = urllib2.urlopen('http://envision.bioe.orst.edu/StudyAreas/WW2100/Outputs/HighClim/Model_Outputs_HighClim_Run0.zip')
+zipdata = response.read()
+with zipfile.ZipFile(StringIO(zipdata), "r") as z:
+    z.extractall(directory_path)
+    
+response = urllib2.urlopen('http://envision.bioe.orst.edu/StudyAreas/WW2100/Outputs/FireSuppress/Model_Outputs_FireSuppress_Run0.zip')
+zipdata = response.read()
+with zipfile.ZipFile(StringIO(zipdata), "r") as z:
+    z.extractall(directory_path)
+    
+response = urllib2.urlopen('http://envision.bioe.orst.edu/StudyAreas/WW2100/Outputs/UrbExpand/Model_Outputs_UrbExpand_Run0.zip')
+zipdata = response.read()
+with zipfile.ZipFile(StringIO(zipdata), "r") as z:
+    z.extractall(directory_path)
+
+response = urllib2.urlopen('http://envision.bioe.orst.edu/StudyAreas/WW2100/Outputs/HighPop/Model_Outputs_HighPop_Run0.zip')
+zipdata = response.read()
+with zipfile.ZipFile(StringIO(zipdata), "r") as z:
+    z.extractall(directory_path)
+    
---
+++ readme.txt
@@ -0,0 +1,3 @@
+Roy Haggerty
+Sept., 2014
+This script downloads the WW2100 Envision csv files and places them in a location that you put in the directory_path.
