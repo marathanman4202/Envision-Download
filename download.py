cbf022ecacc9d09167112b493aa56ac338b2f286
marathanman4202 <roy.haggerty@oregonstate.edu>
2014-09-30 10:24:22 -0700

    Add block of code to read and write names
    zipped filenames to xls

--- download.py
+++ download.py
@@ -8,15 +8,28 @@ Created on Sat Sep 13 21:50:40 2014
 import urllib2
 import zipfile
 from StringIO import StringIO
+import xlwt
 
 # Download latest csv files for Envision, and save them to a local directory.
 
-directory_path = "C:\\Users\\haggertr\\Desktop\\Documents\\work - OSU\\research\\WW2100\\Research\\results2\\files\\"
-#directory_path = "C:\\Users\\haggertr\\Desktop\\Roy\\Research\\WW2100\\Research\\results2\\files\\"
+#directory_path = "C:\\Users\\haggertr\\Desktop\\Documents\\work - OSU\\research\\WW2100\\Research\\results2\\files\\"
+directory_path = "C:\\Users\\haggertr\\Desktop\\Roy\\Research\\WW2100\\Research\\results2\\files\\"
 
 response = urllib2.urlopen('http://envision.bioe.orst.edu/StudyAreas/WW2100/Outputs/Ref/Model_Outputs_Ref_Run0.zip')
 zipdata = response.read()
 with zipfile.ZipFile(StringIO(zipdata), "r") as z:
+    # Reads filenames in zip file and then prints them to an xls file
+    names = z.namelist()
+    book = xlwt.Workbook(encoding="utf-8")
+    sheet1 = book.add_sheet("Reference run files")
+    sheet1.write(0,0, "File name")
+    i = 0
+    for name in names:
+        i += 1
+        sheet1.write(i,0,name)
+    book.save(directory_path+"Filenames_Ref.xls")
+    assert False
+    # ^^^ End of read filenames
     z.extractall(directory_path)
     
 response = urllib2.urlopen('http://envision.bioe.orst.edu/StudyAreas/WW2100/Outputs/LowClim/Model_Outputs_LowClim_Run0.zip')
